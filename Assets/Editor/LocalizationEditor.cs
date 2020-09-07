1cba055ce7726f3facca476329e0053c1fe26df3
baiping <ping.bai@smalltreemedia.com>
2020-09-07 18:03:31 +0800

    no message

---
+++ Assets/Editor/LocalizationEditor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+namespace Localization
+{
+    [CustomEditor(typeof(LocalizationAsset))]
+    public class LocalizationEditor : Editor
+    {
+        LocalizationAsset asset;
+        private void Awake()
+        {
+            asset = (LocalizationAsset)target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+
+            if (GUILayout.Button("Open in editor"))
+            {
+                LocalizationEditorWindow.Init(asset);
+
+            }
+
+        }
+
+    }
+}
---
+++ Assets/LocalizationAsset.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Localization
+{
+    [CreateAssetMenu(fileName = "LocalizationAsset", menuName = "Localization/LocalizationAsset")]
+    public class LocalizationAsset : ScriptableObject
+    {
+        public LanguageInfo[] languageInfos = new LanguageInfo[0];
+        public string[] keys = new string[0];
+    }
+
+    [System.Serializable]
+    public class LanguageInfo
+    {
+        [SerializeField]
+        public SystemLanguage language;
+        [SerializeField]
+        public StringKeyValue[] stringInfo = new StringKeyValue[0];
+
+ 
+        public LanguageInfo()
+        {
+
+        }
+
+        public StringKeyValue GetElement(string key)
+        {
+            for (int i = 0; i < stringInfo.Length; i++)
+            {
+                if (stringInfo[i].key == key)
+                {
+                    return stringInfo[i];
+                }
+            }
+            Debug.LogError("Cant Find the key:" + key);
+            return new StringKeyValue();
+        }
+
+        public string GetValue(string key)
+        {
+            for (int i = 0; i < stringInfo.Length; i++)
+            {
+                if (stringInfo[i].key == key)
+                {
+                    return stringInfo[i].value;
+                }
+            }
+            Debug.LogError("Cant Find the key:" + key);
+            return "";
+        }
+
+        public string GetKey(string value)
+        {
+            for (int i = 0; i < stringInfo.Length; i++)
+            {
+                if (stringInfo[i].value == value)
+                {
+                    return stringInfo[i].key;
+                }
+            }
+            Debug.LogError("Cant Find the value:" + value);
+            return "";
+        }
+
+
+    }
+    [System.Serializable]
+    public struct StringKeyValue
+    {
+        [SerializeField]
+        public string key;
+        [SerializeField]
+        public string value;
+
+        public StringKeyValue(string key1 ,string value1)
+        {
+            key = key1;
+            value = value1;
+
+        }
+
+    }
+}
