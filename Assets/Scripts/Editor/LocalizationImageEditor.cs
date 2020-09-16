af8b8ca1fb7571fafc6c2c44012c9e198b125868
baiping <ping.bai@smalltreemedia.com>
2020-09-16 12:26:06 +0800

    no message

---
+++ Assets/Scripts/Editor/LocalizationImageEditor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+namespace Localization
+{
+    [CustomEditor(typeof(LocalizationImage))]
+    public class LocalizationImageEditor : Editor
+    {
+        LocalizationImage asset;
+        List<string> strs = new List<string>();
+
+        private void Awake()
+        {
+
+            asset = (LocalizationImage)target;
+
+            for (int i = 0; i < LocalizationMgr.Instance.asset.localizationAssetKeys.Length; i++)
+            {
+                if (LocalizationMgr.Instance.asset.localizationAssetKeys[i].localizationType == LocalizationAssetType.sprite)
+                {
+                    strs.Add(LocalizationMgr.Instance.asset.localizationAssetKeys[i].key);
+                }
+
+
+            }
+
+        }
+        public override void OnInspectorGUI()
+        {
+
+            DrawDefaultInspector();
+
+
+            asset.choiceIndex = EditorGUILayout.Popup("Key", asset.choiceIndex, strs.ToArray());
+            asset.SetKey(strs[asset.choiceIndex]);
+
+            asset.GetComponent<Image>().sprite = LocalizationMgr.Instance.GetValue(asset.Key, LocalizationMgr.Instance.CurrLanguage).sprite;
+            EditorUtility.SetDirty(asset);
+        }
+
+    }
+}
